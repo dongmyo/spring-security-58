29ff185aa278a0dd1fb989b5f8cc5a236f24bae6
신동민/NE <dongmyo@nhnent.com>
2023-06-08 15:39:38 +0900

    * spring boot + security

---
+++ src/test/java/com/nhnent/edu/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnent.edu.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
