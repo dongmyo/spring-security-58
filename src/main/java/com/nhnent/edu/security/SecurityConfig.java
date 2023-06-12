70ce0efed8a86d5e621c2d1371721dcf243e9205
신동민/NE <dongmyo@nhnent.com>
2023-06-12 14:36:35 +0900

    * security 설정 #2

--- src/main/java/com/nhnent/edu/security/SecurityConfig.java
+++ src/main/java/com/nhnent/edu/security/SecurityConfig.java
@@ -4,17 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
-// TODO #1 : security config
 @EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig {
-    // TODO #2: `SecurityFilterChain` 을 반환하는 Bean 등록
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+            // TODO #1: 웹 요청 ACL 스프링 표현식
+            .authorizeHttpRequests()
+                .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                .requestMatchers("/private-project/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_MEMBER")
+                .requestMatchers("/project/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+            // TODO #2: 로그인
+            .formLogin()
+                .and()
+            // TODO #3: 로그아웃
+            .logout()
+                .and()
             .headers()
                 .defaultsDisabled()
                 .cacheControl()
@@ -32,6 +46,27 @@ public class SecurityConfig {
             .csrf()
                 .and()
             .build();
+    }
+
+    // TODO #4: InMemoryUserDetailsManager 반환하는 Bean 등록
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+            .password("{noop}admin")
+            .authorities("ROLE_ADMIN")
+            .build();
+
+        UserDetails member = User.withUsername("member")
+            .password("{noop}member")
+            .authorities("ROLE_MEMBER")
+            .build();
+
+        UserDetails guest = User.withUsername("guest")
+            .password("{noop}guest")
+            .authorities("ROLE_GUEST")
+            .build();
+
+        return new InMemoryUserDetailsManager(admin, member, guest);
     }
 
 }
---
+++ src/main/java/com/nhnent/edu/security/WebConfig.java
@@ -0,0 +1,17 @@
+package com.nhnent.edu.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/admin/**").setViewName("admin");
+        registry.addViewController("/project/**").setViewName("project");
+        registry.addViewController("/private-project/**").setViewName("private-project");
+    }
+
+}
