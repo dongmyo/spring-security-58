7f4137d74960c65a3d9d3a761584a537d96b9337
신동민/NE <dongmyo@nhnent.com>
2023-06-14 21:36:00 +0900

    * security 설정 실습

--- src/main/java/com/nhnent/edu/security/SecurityConfig.java
+++ src/main/java/com/nhnent/edu/security/SecurityConfig.java
@@ -1,85 +1,12 @@
 package com.nhnent.edu.security;
 
-import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
-@EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-            .authorizeHttpRequests()
-                .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
-                .requestMatchers("/private-project/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_MEMBER")
-                .requestMatchers("/project/**").authenticated()
-                .anyRequest().permitAll()
-                .and()
-            .requiresChannel()
-                .requestMatchers("/admin/**").requiresSecure()
-                .requestMatchers("/private-project/**").requiresSecure()
-                .requestMatchers("/project/**").requiresSecure()
-                .anyRequest().requiresInsecure()
-                .and()
-            .formLogin()
-                .loginPage("/login/form")
-                .loginProcessingUrl("/login/process")
-                .usernameParameter("id")
-                .passwordParameter("pwd")
-                // TODO #1: login success handler 설정
-                .successHandler(new CustomLoginSuccessHandler())
-                .and()
-            .logout()
-                .logoutUrl("/auth/logout")
-                // TODO #6: 실습 - logout 커스터마이즈
-                //          로그아웃했을 때 SESSION 이라는 이름의 쿠키를 지우고 세션을 invalidate 시켜주세요.
-                .invalidateHttpSession(true)
-                .deleteCookies("SESSION")
-                .and()
-            .headers()
-                .defaultsDisabled()
-                .cacheControl()
-                    .and()
-                .contentTypeOptions()
-                    .and()
-                .xssProtection()
-                    .headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)
-                    .and()
-                .frameOptions()
-                    .sameOrigin()
-                .and()
-            .csrf()
-                .disable()
-            .addFilterBefore(usernameAdjustingFilter(), UsernamePasswordAuthenticationFilter.class)
-            .sessionManagement()
-                .maximumSessions(1)
-                    .maxSessionsPreventsLogin(true)
-                    .and()
-                .and()
-            .build();
-    }
-
-    @Bean
-    public Filter usernameAdjustingFilter() {
-        return new UsernameAdjustingFilter("username");
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(CustomUserDetailsService customUserDetailsService) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(customUserDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-
-        return authenticationProvider;
-    }
+    // TODO #1: Security 설정
 
     @Bean
     public PasswordEncoder passwordEncoder() {
